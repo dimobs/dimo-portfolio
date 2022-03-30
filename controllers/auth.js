e7edeadd06e9d149a699d439ccd842ef15fbaf8f
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-03-31 00:23:12 +0300

    commitment updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,7 +32,7 @@ router.post('/register',
             res.redirect('/');
         } catch (err) {
             res.locals.errors = mapError(err);
-            res.render('register', { title: 'Register', data: { username: req.body.username } });
+            res.render('register', { title: 'Register', data: { username: req.body.username, password: req.body.password } });
         }
     });
 
@@ -43,7 +43,7 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         await req.auth.login(req.body.username, req.body.password);
-     
+
         res.redirect('/paymentHistory');
     } catch (err) {
         console.error(err.message);
@@ -64,8 +64,8 @@ router.get('/userInfo', (req, res) => {
 router.post('/userInfo',
     body('username').trim(),
     body('NewUsername').trim(),
-    // body('password').trim(),
-    // body('repeatPassword').trim(),
+    body('password').trim(),
+    body('repeatPassword').trim(),
     body('NewUsername')
         .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long')
         .isAlphanumeric().withMessage('Username may contain only alphanumeric characters'),
@@ -74,19 +74,19 @@ router.post('/userInfo',
         .isAlphanumeric().withMessage('Password may contain only alphanumeric characters'),
     body('repeatPassword')
         .custom((value, { req }) =>
-            value == req.body.password)           
+            value == req.body.password)
         .withMessage('Password don\'t match'),
     async (req, res) => {
-        // const { errors } = validationResult(req);
+        const { errors } = validationResult(req);
 
         try {
-            // if (errors.length > 0) {
-            //     throw errors;
-            // }
+            if (errors.length > 0) {
+                throw errors;
+            }
             await req.auth.userUpdate(req.body.username, req.body.NewUsername, req.body.password, req.body.repeatPassword);
-                req.auth.logout();
-                res.redirect('/login');
-               } catch (err) {
+            req.auth.logout();
+            res.redirect('/login');
+        } catch (err) {
             res.locals.errors = mapError(err);
             res.render('404', { title: 'User Info', data: { username: req.body.username } });
         }
--- models/Pay.js
+++ models/Pay.js
@@ -1,14 +1,14 @@
-const {Schema, model, Desical, Types:{ObjectId}} = require('mongoose');
+const { Schema, model, Desical, Types: { ObjectId } } = require('mongoose');
 const mongoose = require('mongoose');
 
 
 const paySchema = new Schema({
-    sender: {type: String, require: false, minlength: [3, 'Pay listing name must be at least 3 characters long']},
-    resiver: {type: String, require: false, minlength: [3, 'Pay listing name must be at least 3 characters long']},
-    imageUrl: {type: String},
-    description: {type: String},
-    date: { type: Date, default: Date.now },
-    amount: {type:Number, require: true },  
+    sender: { type: String, require: false, minlength: [3, 'Pay listing name must be at least 3 characters long'] },
+    resiver: { type: String, require: false, minlength: [3, 'Pay listing name must be at least 3 characters long'] },
+    imageUrl: { type: String },
+    description: { type: String },
+    date: { type: String, require: false},
+    amount: { type: Number, require: true, min: 1 },
     isDeleted: { type: Boolean, default: false },
     owner: { type: ObjectId, ref: 'User' }
 });
