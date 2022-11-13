50db105153c45b51c28f594e3f3d222d744785f4
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-11-13 11:27:13 +0200

    greetings script

---
+++ static/js/greetings.js
@@ -0,0 +1,17 @@
+
+export function greet() {
+
+    const today = new Date()
+    const curHr = today.getHours()
+    let timeOftheDay;
+
+    if (curHr < 12) {
+        timeOftheDay = ('good morning')
+    } else if (curHr < 18) {
+        timeOftheDay = ('good afternoon')
+    } else {
+        timeOftheDay = ('good evening')
+    }
+
+    return timeOftheDay;
+}
