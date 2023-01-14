a83179fd3dd85ce591a42f7994006fd625fe10bd
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-01-14 22:13:59 +0200

    db change

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ require('../models/Pay');
 
 const dbName = 'potfolio'
 // const connectionString = `mongodb://localhost:27017/${dbName}`;
-const connectionString = `mongodb+srv://Dimo:1111Dimo@cluster0.zwa68.mongodb.net/?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://Dimo:112233Dimo@cluster0.zwa68.mongodb.net/?retryWrites=true&w=majority`;
 
 
 module.exports = async(app) =>{
