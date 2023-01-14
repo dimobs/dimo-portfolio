0e1d4ca8ea9f2c40dcb394d6662e4fe4dbcd3685
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2023-01-14 21:17:16 +0200

    atlas db connections

--- config/database.js
+++ config/database.js
@@ -5,7 +5,9 @@ require('../models/Pay');
 
 
 const dbName = 'potfolio'
-const connectionString = `mongodb://localhost:27017/${dbName}`;
+// const connectionString = `mongodb://localhost:27017/${dbName}`;
+const connectionString = `mongodb+srv://Dimo:1111Dimo@cluster0.zwa68.mongodb.net/?retryWrites=true&w=majority`;
+
 
 module.exports = async(app) =>{
     try {
